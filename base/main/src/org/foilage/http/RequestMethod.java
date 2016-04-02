cd3eebba7c3532f3693eca08286040c29f7439b9
Fredrik Gustavsson <fgustavsson@gmail.com>
2016-04-02 09:22:42 +0200

    Added http base

---
+++ base/main/src/org/foilage/http/RequestMethod.java
@@ -0,0 +1,19 @@
+package org.foilage.http;
+
+public enum RequestMethod {
+
+    GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE;
+
+    public static RequestMethod findByName(String name) {
+
+        for(RequestMethod requestMethod: RequestMethod.values()) {
+
+            if(requestMethod.name().equalsIgnoreCase(name)) {
+
+                return requestMethod;
+            }
+        }
+
+        throw new IllegalArgumentException("No Request Method with name "+name+" Exists!");
+    }
+}
---
+++ base/main/src/org/foilage/http/exceptions/URLNotFoundException.java
@@ -0,0 +1,8 @@
+package org.foilage.http.exceptions;
+
+public class URLNotFoundException extends Exception {
+
+    public URLNotFoundException(String message) {
+        super(message);
+    }
+}
