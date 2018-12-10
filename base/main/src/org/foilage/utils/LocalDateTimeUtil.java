292fb2243428ce4a19a8772850c6020ed19b1d76
Fredrik Gustavsson <fgustavsson@gmail.com>
2018-12-10 10:01:34 +0100

    Added LocalDateTimeUtil

---
+++ base/main/src/org/foilage/utils/LocalDateTimeUtil.java
@@ -0,0 +1,39 @@
+package org.foilage.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class LocalDateTimeUtil {
+
+    public static Date getAsDate(LocalDateTime dateTime) {
+
+        return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDateTime getFromDate(Date date) {
+
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    public static LocalDateTime toBeginningOfDay(LocalDateTime dateTime) {
+
+        return dateTime.withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
+
+    public static LocalDateTime toEndOfDay(LocalDateTime dateTime) {
+
+        return dateTime.withHour(23).withMinute(59).withSecond(59);
+    }
+
+    public static LocalDateTime toBeginningOfMonth(LocalDateTime dateTime) {
+
+        return dateTime.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
+
+    public static LocalDateTime toEndOfMonth(LocalDateTime dateTime) {
+
+        return dateTime.withDayOfMonth(dateTime.toLocalDate().lengthOfMonth()).withHour(23).withMinute(59).withSecond(59);
+    }
+
+}
